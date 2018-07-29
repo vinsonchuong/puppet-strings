e4e8e0e8adb93aaa1ed8fe61dde494bee4a7c73a
Vinson Chuong <vinsonchuong@gmail.com>
2018-07-28 21:25:25 -0700

    feat(findElement): Support Selenium browsers

--- actions/find-element/index.js
+++ actions/find-element/index.js
@@ -1,42 +1,20 @@
 /* @flow */
 import type { Element } from 'puppet-strings'
 import { branchOnTab, evalInTab } from 'puppet-strings'
+import { By, until } from 'selenium-webdriver'
 import cssToXPath from 'css-to-xpath'
 
 export default branchOnTab({
   async puppeteer(tab, selector: string, text: ?string): Promise<Element> {
     const { puppeteer: { browser, page } } = tab
-    const xpath =
-      typeof text === 'string'
-        ? cssToXPath
-            .parse(selector)
-            .where(cssToXPath.xPathBuilder.text().contains(text))
-            .toXPath()
-        : cssToXPath(selector)
+    const xpath = buildXPath(selector, text)
 
     try {
       const elementHandle = await page.waitForXPath(xpath, {
         visible: true,
         timeout: 5000
       })
-
-      const metadata = await evalInTab(
-        tab,
-        [elementHandle],
-        `
-        const [elementHandle] = arguments
-
-        return {
-          attributes: Array.from(elementHandle.attributes).reduce(
-            (memo, attr) => Object.assign(memo, { [attr.name]: attr.value }),
-            {}
-          ),
-          innerText: elementHandle.innerText,
-          outerHTML: elementHandle.outerHTML
-        }
-        `
-      )
-
+      const metadata = await getElementMetadata(tab, elementHandle)
       return {
         ...metadata,
         puppeteer: { browser, page, elementHandle }
@@ -44,5 +22,52 @@ export default branchOnTab({
     } catch (error) {
       throw new Error('Could not find element')
     }
+  },
+
+  async selenium(tab, selector: string, text: ?string): Promise<Element> {
+    const { selenium: { webDriver } } = tab
+    const xpath = buildXPath(selector, text)
+
+    try {
+      const webElement = await webDriver.wait(
+        until.elementLocated(By.xpath(xpath)),
+        5000
+      )
+      const metadata = await getElementMetadata(tab, webElement)
+      return {
+        ...metadata,
+        selenium: { webDriver, webElement }
+      }
+    } catch (error) {
+      throw new Error('Could not find element')
+    }
   }
 })
+
+function buildXPath(selector, text) {
+  return typeof text === 'string'
+    ? cssToXPath
+        .parse(selector)
+        .where(cssToXPath.xPathBuilder.text().contains(text))
+        .toXPath()
+    : cssToXPath(selector)
+}
+
+function getElementMetadata(tab, element) {
+  return evalInTab(
+    tab,
+    [element],
+    `
+    const [element] = arguments
+
+    return {
+      attributes: Array.from(element.attributes).reduce(
+        (memo, attr) => Object.assign(memo, { [attr.name]: attr.value }),
+        {}
+      ),
+      innerText: element.innerText,
+      outerHTML: element.outerHTML
+    }
+    `
+  )
+}
---
+++ actions/find-element/selenium.test.js
@@ -0,0 +1,135 @@
+/* @flow */
+import test from 'ava'
+import {
+  withBrowser,
+  withDirectory,
+  writeFile
+} from 'puppet-strings/test/helpers'
+import { getTabs, navigate, findElement } from 'puppet-strings'
+
+withBrowser({ perTest: true, type: 'firefox' })
+withDirectory()
+
+test('finding an element', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+    <p>Hello World!</p>
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+  const element = await findElement(tab, 'p')
+  t.is(element.innerText, 'Hello World!')
+})
+
+test('failing to find an element', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+
+  await t.throws(findElement(tab, 'p'), 'Could not find element')
+})
+
+test('finding an element containing text', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+    <p>Hello World!</p>
+    <p>Other Stuff</p>
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+
+  const element = await findElement(tab, 'p', 'Other Stuff')
+  t.is(element.innerText, 'Other Stuff')
+})
+
+test('failing to find an element containing text', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+    <p>Hello World!</p>
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+
+  await t.throws(findElement(tab, 'p', 'Other Stuff'), 'Could not find element')
+})
+
+test('waiting for an element to appear', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+    <script>
+      setTimeout(() => {
+        document.body.innerHTML = '<p>Hello World!</p>'
+      }, 2000)
+    </script>
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+
+  const element = await findElement(tab, 'p')
+  t.is(element.innerText, 'Hello World!')
+})
+
+test('failing to find an element after 5 seconds', async t => {
+  const { directory, browser } = t.context
+
+  const htmlPath = await writeFile(
+    directory,
+    'index.html',
+    `
+    <!doctype html>
+    <meta charset="utf-8">
+    <script>
+      setTimeout(() => {
+        document.body.innerHTML = '<p>Hello World!</p>'
+      }, 10000)
+    </script>
+  `
+  )
+
+  const [tab] = await getTabs(browser)
+  await navigate(tab, `file://${htmlPath}`)
+
+  await t.throws(findElement(tab, 'p'), 'Could not find element')
+})
